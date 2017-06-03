830646f2b0dc9b0e8722c5dd7b9dec1fcbda0051
Hygor Souza <souza.hygor13@gmail.com>
2017-06-03 12:20:10 -0300

    Initial commit

---
+++ app/src/main/java/br/usjt/ftce/desmob/clientev1/model/ClienteDb.java
@@ -0,0 +1,71 @@
+package br.usjt.ftce.desmob.clientev1.model;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+
+/**
+ * Created by asbonato on 9/20/15.
+ */
+public class ClienteDb {
+    ClienteDbHelper dbHelper;
+
+    public static final String CLIENTE = "cliente";
+
+    public ClienteDb(Context context){
+        dbHelper = new ClienteDbHelper(context);
+    }
+
+    public void insereCor(ArrayList<Cliente> clientes){
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        for(Cliente cliente:clientes) {
+            ContentValues values = new ContentValues();
+
+            values.put(ClienteContract.ClienteEntry.COLUMN_NAME_CLIENTE_NOME, cliente.getNome());
+            values.put(ClienteContract.ClienteEntry.COLUMN_NAME_CLIENTE_FONE, cliente.getFone());
+            values.put(ClienteContract.ClienteEntry.COLUMN_NAME_CLIENTE_EMAIL, cliente.getEmail());
+
+
+            db.insert(ClienteContract.ClienteEntry.TABLE_NAME, null, values);
+        }
+    }
+
+
+
+    public ArrayList<Cliente>  selecionaClientes(){
+        ArrayList<Cliente> lista = new ArrayList<Cliente>();
+
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+        String[] colunas = {ClienteContract.ClienteEntry._ID,
+                ClienteContract.ClienteEntry.COLUMN_NAME_CLIENTE_NOME ,
+                ClienteContract.ClienteEntry.COLUMN_NAME_CLIENTE_FONE ,
+                ClienteContract.ClienteEntry.COLUMN_NAME_CLIENTE_EMAIL };
+
+        String ordem = ClienteContract.ClienteEntry.COLUMN_NAME_CLIENTE_NOME;
+
+        Cursor c = db.query(ClienteContract.ClienteEntry.TABLE_NAME, colunas, null, null, ordem, null, null );
+
+        while(c.moveToNext()){
+            int id = c.getInt(c.getColumnIndex(ClienteContract.ClienteEntry._ID));
+            String nome = c.getString(c.getColumnIndex(ClienteContract.ClienteEntry.COLUMN_NAME_CLIENTE_NOME));
+            String fone = c.getString(c.getColumnIndex(ClienteContract.ClienteEntry.COLUMN_NAME_CLIENTE_FONE));
+            String email = c.getString(c.getColumnIndex(ClienteContract.ClienteEntry.COLUMN_NAME_CLIENTE_NOME));
+
+            Cliente cliente = new Cliente(id,nome,fone, email);
+
+
+            lista.add( cliente );
+        }
+
+        c.close();
+
+        return lista;
+    }
+
+
+}
